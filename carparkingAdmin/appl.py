05442fd942c2b9a63eac1b8974734358d5cf2c37
Bhumika Alhat <bhumikaalhat@gmail.com>
2025-09-09 12:20:11 +0530

    Create appl.py

---
+++ carparkingAdmin/appl.py
@@ -0,0 +1,10 @@
+"""
+    ######### CAR PARKING ADMIN APPLICATION ##########
+    AUTHER :    Bhumika Alhat
+"""
+
+from Application import *
+from DBConnection import *
+
+app = App()
+app.__start__()
